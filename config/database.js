4854361d5b0bb30e168534aa51ec3b551e020e62
Lehel Kovach <lehelkovach@gmail.com>
2015-01-11 13:18:53 -0800

    added remote database connection URIs

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
 // config/database.js
 module.exports = {
-	//'barter_url' : 'mongodb://lehel:Kovach789@oceanic.mongohq.com:10050/barter' ,
-	'barter_url' : 'mongodb://localhost/barter' ,
-	//'climbtime_url' : 'mongodb://lehel:Kovach789@paulo.mongohq.com:10011/climbtime'
-	'climbtime_url' : 'mongodb://localhost/climbtime'
+	'barter_url' : 'mongodb://lehel:Kovach789@oceanic.mongohq.com:10050/barter' ,
+	//'barter_url' : 'mongodb://localhost/barter' ,
+	'climbtime_url' : 'mongodb://lehel:Kovach789@paulo.mongohq.com:10011/climbtime',
+	//'climbtime_url' : 'mongodb://localhost/climbtime',
+	'redis_url' : 	'redis://redistogo:35a5a60f5b75d1fa46b1d798ffa4e46b@mummichog.redistogo.com:10138/'
+
 };
 
 
